b0f1cd84e390420a2a7a26f408764436f8b12ff8
judy2112129 <judy2112129@miuegypt.edu.eg>
2025-01-19 12:58:42 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require("express");
 const {
-  cartController,
-  getCartController,
-  wishlistController,
-  getWishlistController,
+	cartController,
+	getCartController,
+	wishlistController,
+	getWishlistController,
+	getProfile,
+	updateProfile,
+	getOrders,
+	updateOrder,
+	deleteOrder,
 } = require("../controllers/user.controller");
 const UserPrivileges = require("../middlewares/isUser");
 var router = express.Router();
@@ -12,5 +17,11 @@ router.post("/cart/:id", UserPrivileges, cartController);
 router.get("/cart", UserPrivileges, getCartController);
 router.post("/wishlist/:id", UserPrivileges, wishlistController);
 router.get("/wishlist", UserPrivileges, getWishlistController);
+
+router.get("/profile", UserPrivileges, getProfile);
+router.post("/profile/update", UserPrivileges, updateProfile);
+router.get("/orders", UserPrivileges, getOrders);
+router.post("/orders/:id/edit", UserPrivileges, updateOrder);
+router.post("/orders/:id/delete", UserPrivileges, deleteOrder);
 
 module.exports = router;
