47508eee5a1bb6ab1d9a233c5fb3f1b5b134d0ac
amlelgedawy <aml2202037@miuegypt.edu.eg>
2025-01-20 07:17:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,18 +7,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const expressLayouts = require("express-ejs-layouts");
 const { checkUser } = require("./middlewares/auth.middleware");
-
+const upload = require("./middlewares/fileUpload");
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => {
+		console.log(err);
+		process.exit(1);
+	});
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var paymentsRouter = require("./routes/payments");
+var adminRouter = require("./routes/admin");
+var logoutRouter = require("./routes/logout");
 
 var app = express();
 app.use(cors());
@@ -32,7 +34,7 @@ app.set("layout", "layouts/layout");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
@@ -40,28 +42,39 @@ app.use(checkUser);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/payments", paymentsRouter);
+app.use("/admin", adminRouter);
+app.use("/auth", logoutRouter); 
 
-// Middleware to handle errors
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
+});
+app.use((req, res, next) => {
+	if (req.originalUrl.startsWith("/admin")) {
+		res.locals.layout = false;
+	}
+	next();
+});
+app.use((err, req, res, next) => {
+	const status = err.status || 500;
+	const message = err.message || "Something went wrong";
+	const description =
+		status === 404
+			? "The page you are looking for does not exist."
+			: "There was an error processing your request.";
+
+	res.status(status).render("error", {
+		layout: false,
+		errorStatus: status,
+		errorMessage: message,
+		errorDescription: description,
+	});
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong";
-  const description =
-    status === 404
-      ? "The page you are looking for does not exist."
-      : "There was an error processing your request.";
-
-  res.status(status).render("error", {
-    errorStatus: status,
-    errorMessage: message,
-    errorDescription: description,
-  });
+const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
